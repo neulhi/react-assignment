5b9e439f5909929b18d7fa502bb5b0659b0e4c59
neulhi <neu1hi.work@gmail.com>
2025-01-30 00:18:28 +0900

    [수정] app.tsx에 렌더링

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,12 @@
+import SignUpForm from './components/SignUp-In/SignUpForm';
+import SignInForm from './components/SignUp-In/SignInForm';
+
 function App() {
   return (
-    <div className="prose">
-      <header>
-        <h1 lang="en">Custom Vite Template</h1>
-      </header>
-      <main>
-        <p>React 프로젝트 시작을 위한 사용자 정의 템플릿</p>
-      </main>
-      <footer>
-        <small lang="en">
-          &copy; {new Date().getFullYear()} EUID. All rights reserved.
-        </small>
-      </footer>
-    </div>
+    <>
+      <SignUpForm />
+      <SignInForm />
+    </>
   );
 }
 
